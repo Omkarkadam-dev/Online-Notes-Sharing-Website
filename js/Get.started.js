922898e5542061cd52468527e8e60054e1607a3b
Omkar Kadam <omkarkadam1115@gmail.com>
2025-07-30 15:41:10 +0530

    align format

--- js/Get.started.js
+++ js/Get.started.js
@@ -1,16 +1,16 @@
 // Simulate navigation
 function navigateTo(page) {
-  switch(page) {
-    case 'signup':
-      alert("Redirecting to Sign Up page...");
-      break;
-    case 'login':
-      alert("Redirecting to Login page...");
-      break;
-    case 'upload':
-      alert("Redirecting to Upload Notes...");
-      break;
-    default:
-      alert("Page not found.");
-  }
+    switch (page) {
+        case 'signup':
+            alert("Redirecting to Sign Up page...");
+            break;
+        case 'login':
+            alert("Redirecting to Login page...");
+            break;
+        case 'upload':
+            alert("Redirecting to Upload Notes...");
+            break;
+        default:
+            alert("Page not found.");
+    }
 }
